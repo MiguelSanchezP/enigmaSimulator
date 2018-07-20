6992f85227e0d3a89245228b44fcd68f0eb9f058
miguelsanchez2003 <miguelsanchez1703@gmail.com>
2018-07-20 23:35:09 +0200

    Looks like solved continue with the AllLettersPane, Was all initializable?

--- src/com/miguelsanchez/NewPlugboardController.java
+++ src/com/miguelsanchez/NewPlugboardController.java
@@ -1,31 +1,50 @@
 package com.miguelsanchez;
 
-import javafx.scene.control.RadioButton;
-import javafx.scene.control.Tooltip;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.layout.GridPane;
 
-import static com.miguelsanchez.auxiliars.AllLettersPane.getAllLettersPane;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class NewPlugboardController {
+import static com.miguelsanchez.auxiliars.AllLettersPane.generateTheAllLettersPane;
 
-    static GridPane configureTheGridPane () {
-        GridPane PlugConfigurationPane = new GridPane();
-        RadioButton tempRB1 = new RadioButton();
-        tempRB1.setSelected(true);
-        tempRB1.setText("Double direction wire");
-        Tooltip tooltipRB1 = new Tooltip();
-        tooltipRB1.setText("Button Up: the wire goes from A to B and from B to A; Button Down: the wire only goes from A to B");
-        tempRB1.setTooltip(tooltipRB1);
-        PlugConfigurationPane.add(tempRB1, 0, 0);
-        GridPane gp = getAllLettersPane();
-        System.out.println(gp.toString());
-        PlugConfigurationPane.add(gp, 0, 1);
-        RadioButton tempRB2 = new RadioButton();
-        tempRB2.setText("Configure Later");
-        Tooltip tooltipRB2 = new Tooltip ();
-        tooltipRB2.setText("Button Up: Configure the plugboard later; Button Down: Configure the plugboard now");
-        tempRB2.setTooltip (tooltipRB2);
-        PlugConfigurationPane.add(tempRB2, 0, 2);
-        return PlugConfigurationPane;
+public class NewPlugboardController implements Initializable {
+
+//    static GridPane configureTheGridPane () {
+//        GridPane PlugConfigurationPane = new GridPane();
+//        RadioButton tempRB1 = new RadioButton();
+//        tempRB1.setSelected(true);
+//        tempRB1.setText("Double direction wire");
+//        Tooltip tooltipRB1 = new Tooltip();
+//        tooltipRB1.setText("Button Up: the wire goes from A to B and from B to A; Button Down: the wire only goes from A to B");
+//        tempRB1.setTooltip(tooltipRB1);
+//        PlugConfigurationPane.add(tempRB1, 0, 0);
+//        GridPane gp = getAllLettersPane();
+//        System.out.println(gp.toString());
+//        PlugConfigurationPane.add(gp, 0, 1);
+//        RadioButton tempRB2 = new RadioButton();
+//        tempRB2.setText("Configure Later");
+//        Tooltip tooltipRB2 = new Tooltip ();
+//        tooltipRB2.setText("Button Up: Configure the plugboard later; Button Down: Configure the plugboard now");
+//        tempRB2.setTooltip (tooltipRB2);
+//        PlugConfigurationPane.add(tempRB2, 0, 2);
+//        return PlugConfigurationPane;
+//    }
+
+    @FXML
+    private GridPane PlugConfigurationPane;
+
+    @Override
+    public void initialize (URL location, ResourceBundle resources) {
+        configureTheGridPane();
+    }
+    
+    @FXML
+    void configureTheGridPane () {
+        GridPane gp = generateTheAllLettersPane();
+        GridPane gp2 = PlugConfigurationPane;
+        System.out.println(gp);
+        System.out.println(gp2);
     }
 }
--- src/com/miguelsanchez/auxiliars/AllLettersPane.java
+++ src/com/miguelsanchez/auxiliars/AllLettersPane.java
@@ -1,5 +1,6 @@
 package com.miguelsanchez.auxiliars;
 
+import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
@@ -8,19 +9,60 @@ public class AllLettersPane {
     private static String alphabet = "A B C D E F G H I J K L M N O P Q R S T U V W X Y Z";
     private static String[] numberToLetter = alphabet.split(" ");
 
-    private static GridPane generateTheAllLettersPane () {
+    public static GridPane generateTheAllLettersPane () {
         GridPane allLettersPane = new GridPane();
         boolean finish = false;
         boolean oddRound = true;
         int r = 0;
+        int round = 0;
         while (!finish) {
             for (int c = 0; c < 10; c++) {
                 if (oddRound) {
-                    String text = numberToLetter[((r*10+c)/2)];
+                    String text = numberToLetter[(round/2)];
                     text += ":";
                     Label tempL = new Label();
                     tempL.setText(text);
                     allLettersPane.add(tempL, c, r);
+                    oddRound = false;
+                }else{
+                    String text;
+                    if (r>0) {
+                        text = numberToLetter[(round/ 2)];
+                    }else{
+                        text = numberToLetter[(round/2)];
+                    }
+                    TextField tempTF = new TextField();
+                    tempTF.setPrefWidth(30);
+                    tempTF.setPromptText(text);
+                    allLettersPane.add(tempTF, c, r);
+                    oddRound = true;
+                    round++;
+                }
+                if (round == (numberToLetter.length*2)) {
+                    finish = true;
+                    break;
+                }
+            }
+            r++;
+        }
+        System.out.println(allLettersPane);
+        return allLettersPane;
+    }
+
+    @FXML
+    public static void setAGridPane (GridPane gp) {
+        boolean finish = false;
+        boolean oddRound = true;
+        int r = 0;
+        int round = 0;
+        while(!finish) {
+            for (int c = 0; c<10; c++) {
+                if (oddRound) {
+                    String text = numberToLetter[(r*10+c)/2];
+                    text +=":";
+                    Label tempL = new Label();
+                    tempL.setText(text);
+                    gp.add(tempL, c, r);
                     oddRound = false;
                 }else{
                     String text;
@@ -32,17 +74,18 @@ public class AllLettersPane {
                     TextField tempTF = new TextField();
                     tempTF.setPrefWidth(30);
                     tempTF.setPromptText(text);
-                    allLettersPane.add(tempTF, c, r);
+                    gp.add(tempTF, c, r);
                     oddRound = true;
+                    round++;
                 }
-                if ((r*10+c*2) == (numberToLetter.length*2)) {
+                if (round == (numberToLetter.length*2)) {
                     finish = true;
                     break;
                 }
             }
             r++;
         }
-        return allLettersPane;
+
     }
 
     public static GridPane getAllLettersPane () {
