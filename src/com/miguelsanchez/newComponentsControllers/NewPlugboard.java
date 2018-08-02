55c6ec8945b6028470fe76c2fc649ea1cf5e98b7
miguelsanchez2003 <miguelsanchez1703@gmail.com>
2018-08-02 12:03:37 +0200

    All comboboxes start initialized with the characters of the alphabet

--- src/com/miguelsanchez/newComponentsControllers/NewPlugboard.java
+++ src/com/miguelsanchez/newComponentsControllers/NewPlugboard.java
@@ -1,8 +1,12 @@
 package com.miguelsanchez.newComponentsControllers;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.RadioButton;
 import javafx.scene.layout.GridPane;
+
+import static com.miguelsanchez.Controller.getAlphabetComponents;
+import static com.miguelsanchez.auxiliars.FillComboBox.alphabetsContentFill;
 
 public class NewPlugboard {
 
@@ -10,8 +14,15 @@ public class NewPlugboard {
     private GridPane PlugConfigurationPane;
     @FXML
     private RadioButton RBConfigureLater, RBDoubleWire;
+    @FXML
+    private ComboBox <String> CBFirstCharacter, CBSecondCharacter;
 
     private boolean doubleWire;
+
+    public void initialize () {
+        alphabetsContentFill(CBFirstCharacter, getAlphabetComponents());
+        alphabetsContentFill(CBSecondCharacter, getAlphabetComponents());
+    }
 
 
     @FXML
