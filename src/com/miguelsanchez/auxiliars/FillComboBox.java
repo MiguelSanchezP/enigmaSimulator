6551d96a23ac0b50372ce591c913670a1fe2cb9c
miguelsanchez2003 <miguelsanchez1703@gmail.com>
2018-08-02 12:02:52 +0200

    Added the case for the characters of an alphabet

--- src/com/miguelsanchez/auxiliars/FillComboBox.java
+++ src/com/miguelsanchez/auxiliars/FillComboBox.java
@@ -1,10 +1,25 @@
 package com.miguelsanchez.auxiliars;
 
 import com.miguelsanchez.components.Alphabet;
+import javafx.beans.InvalidationListener;
+import javafx.collections.FXCollections;
+import javafx.collections.ListChangeListener;
+import javafx.collections.ObservableList;
 import javafx.scene.control.ComboBox;
+
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
 
 public class FillComboBox {
     public static void alphabetsFill (ComboBox<String> cb) {
         cb.setItems(Alphabet.getAlphabetsOL());
+    }
+
+    public static void alphabetsContentFill (ComboBox<String> cb, String[] s) {
+        ObservableList<String> components = FXCollections.observableArrayList();
+        components.addAll(s);
+        cb.setItems(components);
     }
 }
