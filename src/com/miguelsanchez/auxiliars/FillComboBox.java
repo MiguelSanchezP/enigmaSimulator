22c67b10e515b2c7bbc5d8ea33b39ed1dba9f57c
miguelsanchez2003 <miguelsanchez1703@gmail.com>
2018-08-03 17:44:42 +0200

    Deleted unused imports

--- src/com/miguelsanchez/auxiliars/FillComboBox.java
+++ src/com/miguelsanchez/auxiliars/FillComboBox.java
@@ -1,16 +1,9 @@
 package com.miguelsanchez.auxiliars;
 
 import com.miguelsanchez.components.Alphabet;
-import javafx.beans.InvalidationListener;
 import javafx.collections.FXCollections;
-import javafx.collections.ListChangeListener;
 import javafx.collections.ObservableList;
 import javafx.scene.control.ComboBox;
-
-import java.util.Collection;
-import java.util.Iterator;
-import java.util.List;
-import java.util.ListIterator;
 
 public class FillComboBox {
     public static void alphabetsFill (ComboBox<String> cb) {
