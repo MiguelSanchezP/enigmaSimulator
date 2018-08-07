b657bd6872628fcccd125392c4da8fc7b515fdcc
miguelsanchez2003 <miguelsanchez1703@gmail.com>
2018-08-07 14:08:37 +0200

    made the callback a method

--- src/com/miguelsanchez/newComponentsControllers/NewPlugboard.java
+++ src/com/miguelsanchez/newComponentsControllers/NewPlugboard.java
@@ -19,7 +19,7 @@ public class NewPlugboard {
     @FXML
     private RadioButton RBConfigureLater, RBDoubleWire;
     @FXML
-    private ComboBox <String> CBFirstCharacter, CBSecondCharacter;
+    private ComboBox<String> CBFirstCharacter, CBSecondCharacter;
     @FXML
     private Button BConfirmation;
     @FXML
@@ -27,7 +27,7 @@ public class NewPlugboard {
 
     private HashMap<String, String> plugConfiguration = new HashMap<>();
 
-    public void initialize () {
+    public void initialize() {
         alphabetsContentFill(CBFirstCharacter, getAlphabetComponents());
         alphabetsContentFill(CBSecondCharacter, getAlphabetComponents());
         CBFirstCharacter.setPromptText("A");
@@ -36,54 +36,57 @@ public class NewPlugboard {
 
 
     @FXML
-    private void handleRBConfigureLater () {
+    private void handleRBConfigureLater() {
         if (RBConfigureLater.isSelected()) {
             RBDoubleWire.setDisable(true);
-        }else{
+        } else {
             PlugConfigurationPane.getChildren().get(0).setDisable(false);
             PlugConfigurationPane.getChildren().get(2).setDisable(false);
         }
     }
+
     @FXML
-    private void handleBConfirmation () {
+    private void handleBConfirmation() {
         if (RBDoubleWire.isSelected()) {
             String cA = CBFirstCharacter.getValue();
             String cB = CBSecondCharacter.getValue();
             plugConfiguration.put(cA, cB);
             plugConfiguration.put(cB, cA);
-            CBSecondCharacter.getItems().remove(cA);
-            CBSecondCharacter.getItems().remove(cB);
-            CBFirstCharacter.setCellFactory(new Callback<ListView<String>, ListCell<String>>() {
-                @Override
-                public ListCell<String> call(ListView<String> param) {
-                     ListCell<String> cell = new ListCell<String>() {
-                        @Override
-                        protected void updateItem (String item, boolean empty) {
-                            super.updateItem(item, empty);
-                            if (item != null) {
-                                setText(item);
-                                if (item.equals(cA) || item.equals(cB)) {
-                                    setTextFill (Color.RED);
-                                }
-                            }else{
-                                setText(null);
-                            }
-                        }
-                    };
-                     return cell;
-                }
-            });
+            CBFirstCharacter.setCellFactory(getColorRed(cA));
+            CBSecondCharacter.setCellFactory(getColorRed(cA));
         }
     }
- /* add the an undo button to configure another wiring*/
+
     @FXML
-    private void handleCBFirstCharacter () {
+    private void handleCBFirstCharacter() {
         String cA = CBFirstCharacter.getValue();
         if (plugConfiguration.containsKey(cA)) {
             CBSecondCharacter.setValue(plugConfiguration.get(cA));
             LInformation.setText("Value already defined");
-        }else{
+        } else {
             CBSecondCharacter.setValue(cA);
         }
+    }
+
+    private Callback<ListView<String>, ListCell<String>> getColorRed (String cA) {
+        return new Callback<ListView<String>, ListCell<String>>() {
+            @Override
+            public ListCell<String> call(ListView<String> param) {
+                return new ListCell<String>() {
+                    @Override
+                    protected void updateItem(String item, boolean empty) {
+                        super.updateItem(item, empty);
+                        if (item != null) {
+                            setText(item);
+                            if (item.equals(cA)) {
+                                setTextFill(Color.RED);
+                            }
+                        } else {
+                            setText(null);
+                        }
+                    }
+                };
+            }
+        };
     }
 }
